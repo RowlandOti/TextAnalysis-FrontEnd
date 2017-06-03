3367d2358636d8830a4b4446cbcd6d7e9047d81a
MichaelNJ <kamaumichael90@gmail.com>
2017-06-03 17:35:06 +0300

    Good

---
+++ build/Search.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import {render} from 'react-dom';
+import { GridList, GridTile } from 'material-ui/GridList';
+import TextField from 'material-ui/TextField';
+import AppBar from 'material-ui/AppBar';
+//import {deepOrange500} from 'material-ui/styles/colors';
+//import {orange500} from 'material-ui/styles/colors';
+//import muiThemeable from 'material-ui/styles/muiThemeable';
+//import getMuiTheme from 'material-ui/styles/ge';
+
+const Bar = () =>(
+    <div>
+        <AppBar title="Analyze"/>
+    </div>
+);
+
+
+export default Bar;
